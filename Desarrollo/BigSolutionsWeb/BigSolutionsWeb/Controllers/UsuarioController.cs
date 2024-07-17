4c0d6613ec273fba3b7d15e5feead727c59ecb69
Fabian-Montero <fabianja0477@gmail.com>
2024-07-17 17:00:55 -0600

    Plantilla y estructura del proyecto

---
+++ Desarrollo/BigSolutionsWeb/BigSolutionsWeb/Controllers/UsuarioController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigSolutionsWeb.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        public IActionResult ConsultarUsuariosPrueba()
+        {
+            return View();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# BigSolutions
+# Big Solutions
+
+## Sistema web de bocetos mediante inteligencia artificial y herramienta interna para la empresa Big Solutions
+
+Proyecto de la Universidad Fidélitas para los cursos de:
+
+- SC-603 Análisis y Modelado de Requerimientos 
+- SC-702 Diseño y desarrollo de sistemas
+- SC-803 Implantación de Sistemas
