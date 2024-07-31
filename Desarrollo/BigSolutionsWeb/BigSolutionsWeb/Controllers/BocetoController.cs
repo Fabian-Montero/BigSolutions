815236832b43612fb478498d923ce623b8d6e3e0
Fabian-Montero <fabianja0477@gmail.com>
2024-07-30 21:15:10 -0600

    Bocetos, y cotizaciones

---
+++ Desarrollo/BigSolutionsWeb/BigSolutionsWeb/Controllers/BocetoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigSolutionsWeb.Controllers
+{
+    public class BocetoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarProductosBoceto()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarBocetosCliente()
+        {
+            return View();
+        }
+    }
+}
