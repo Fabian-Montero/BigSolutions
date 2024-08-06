aea1c791981323074f0f7fd3982d28e90849b420
Mel10vg <113725871+Mel10vg@users.noreply.github.com>
2024-08-06 00:08:46 -0600

    Contabilidad

---
+++ Desarrollo/BigSolutionsWeb/BigSolutionsWeb/Controllers/ContabilidadController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigSolutionsWeb.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+        //Facturas Venta
+        public IActionResult ConsultarFacturasVenta()
+        {
+            return View();
+        }
+        public IActionResult AgregarFacturasVenta()
+        {
+            return View();
+        }
+
+        //Facturas Pendientes
+        public IActionResult ConsultarFacturasPendientes()
+        {
+            return View();
+        }
+        public IActionResult AgregarFacturasPendientes()
+        {
+            return View();
+        }
+        public IActionResult EditarFacturasPendientes()
+        {
+            return View();
+        }
+
+        //Comprobantes de Pago
+        public IActionResult ConsultarComprobantesDePago()
+        {
+            return View();
+        }
+        public IActionResult AgregarComprobantesDePago()
+        {
+            return View();
+        }
+        public IActionResult ComprobantesDePago()
+        {
+            return View();
+        }
+    }
+}
